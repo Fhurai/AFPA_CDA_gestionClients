5553471f7eacc928092d1040d0c7c73a1d994b08
Fhurai <kulu57@gmail.com>
2025-02-11 12:59:44 +0100

    Ajout contrats dans le script SQL

--- conception/gestionclients.sql
+++ conception/gestionclients.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : jeu. 06 fév. 2025 à 09:04
+-- Généré le : mar. 11 fév. 2025 à 08:02
 -- Version du serveur : 9.1.0
 -- Version de PHP : 8.3.14
 
@@ -35,7 +35,7 @@ CREATE TABLE IF NOT EXISTS `adresses` (
   `codePostal` varchar(5) NOT NULL,
   `ville` varchar(30) NOT NULL,
   PRIMARY KEY (`identifiant`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `adresses`
@@ -47,7 +47,8 @@ INSERT INTO `adresses` (`identifiant`, `numRue`, `nomRue`, `codePostal`, `ville`
 (3, '3', 'rue des Michottes', '54000', 'Nancy'),
 (4, '28', 'Boulevard Albert 1er', '54000', 'NANCY'),
 (5, '80', 'QUAI VOLTAIRE', '95870', 'BEZONS'),
-(6, '276', 'AVENUE DU PRESIDENT WILSON', '93210', 'SAINT-DENIS');
+(6, '276', 'AVENUE DU PRESIDENT WILSON', '93210', 'SAINT-DENIS'),
+(7, '25', 'rue Serpenoise', '57000', 'Metz');
 
 -- --------------------------------------------------------
 
@@ -93,7 +94,16 @@ CREATE TABLE IF NOT EXISTS `contrats` (
   `idClient` int NOT NULL,
   PRIMARY KEY (`idContrat`),
   KEY `idClient` (`idClient`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `contrats`
+--
+
+INSERT INTO `contrats` (`idContrat`, `libelleContrat`, `montant`, `idClient`) VALUES
+(1, 'Contrat test Efluid', 1500.0000, 1),
+(2, 'Contrat Interim Efluid', 45000.0000, 1),
+(3, 'Contrat Pro Happiso', 75000.0000, 3);
 
 -- --------------------------------------------------------
 
@@ -114,7 +124,7 @@ CREATE TABLE IF NOT EXISTS `prospects` (
   PRIMARY KEY (`identifiant`),
   UNIQUE KEY `raisonSociale` (`raisonSociale`),
   KEY `idAdresse` (`idAdresse`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `prospects`
@@ -123,7 +133,8 @@ CREATE TABLE IF NOT EXISTS `prospects` (
 INSERT INTO `prospects` (`identifiant`, `raisonSociale`, `telephone`, `mail`, `commentaires`, `dateProspection`, `prospectInteresse`, `idAdresse`) VALUES
 (1, 'CGI', '0388553370', 'contact@cgi.com', NULL, '2025-10-30', 0, 4),
 (2, 'ATOS', '0173260000', 'contact@atos.fr', NULL, '2024-05-28', 0, 5),
-(3, 'Expectra', '0387172390', 'contact@expectra.fr', NULL, '2024-10-10', 1, 6);
+(3, 'Expectra', '0387172390', 'contact@expectra.fr', NULL, '2024-10-10', 1, 6),
+(4, 'Artistsnclients', '0354626298', 'luku@free.fr', 'test', '2023-10-15', 0, 7);
 
 --
 -- Contraintes pour les tables déchargées
